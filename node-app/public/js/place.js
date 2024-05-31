6a06a03d016f217337a39db3d5512227dbc80774
MaiNatori <a21.eg3p@g.chuo-u.ac.jp>
2024-05-31 15:05:14 +0900

    5/31

--- node-app/public/js/place.js
+++ node-app/public/js/place.js
@@ -1,29 +1,55 @@
+let tripDays = 2;
+let nextDayNumber = 2;
+
 function addNewDay() {
+    
     // 追加される日程ボックスを作成
     const input = document.querySelector(".input-place");
     const div = document.createElement('div');
         div.classList.add('day');
-    const h3 = document.createElement("h3");
-        const dayNumber =  document.querySelectorAll('.day').length + 1; // 現在の日程数に1を足して次の日程を計算
-        h3.innerHTML = `${dayNumber}日目`;
-    const labelStart = document.createElement("label");
-        labelStart.innerHTML = "出発地: ";
-    const labelGoal = document.createElement("label");
-        labelGoal.innerHTML = "到着地: ";
-    const button = document.createElement("input");
-        button.setAttribute("type", "submit");
-        button.setAttribute("method", "post");
-        button.setAttribute("name", "confirm");
-        button.setAttribute("value", "確定");
-        button.classList.add("button");
+    div.innerHTML = `
+        <h3>${nextDayNumber}日目</h3>
+        <label>出発地: </label>
+        <label>到着地: </label>
+    `;
+    const lastDay = document.querySelector('#last-day');
+    input.insertBefore(div, lastDay);
 
-    div.appendChild(h3);
-    div.appendChild(labelStart);
-    div.appendChild(labelGoal);
-    div.appendChild(button);
-    input.appendChild(div);
-    
-    // plus-buttonの前に新しい日程ボックスを挿入
-    const plusButton = document.querySelector('.plus-button');
-    plusButton.parentNode.insertBefore(dayElement, plusButton);
-};
+    tripDays++;
+    document.getElementById("trip-days").textContent = tripDays;
+
+    nextDayNumber++;
+
+    updateDayNumbers();
+}
+
+function deleteDay(button) {
+    const days = document.querySelectorAll('.day');
+    if (days.length > 2) {
+        const lastDay = days[days.length -2];
+        lastDay.remove();
+
+        tripDays--;
+        document.getElementById('trip-days').textContent = tripDays;
+
+        nextDayNumber--;
+
+        updateDayNumbers();
+    }
+
+    const dayDiv = button.parentElement;
+    dayDiv.remove();
+    updateDayNumbers();
+}
+
+function updateDayNumbers() {
+    const days = document.querySelectorAll('.day');
+    days.forEach((day, index) => {
+        const h3 = day.querySelector('h3.number');
+        if (index === days.length - 1) {
+            h3.innerHTML = "最終日";
+        } else {
+            h3.innerHTML = `${index + 1}日目`;
+        }
+    });
+}
